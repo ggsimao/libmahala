637aeb3dee7bf9dd2b07aef5278c7fae425dd439
ggsimao <cd_giulio@hotmail.com>
2021-04-16 05:04:58 -0300

    added messages for installing in python

--- install_lib.py
+++ install_lib.py
@@ -2,4 +2,8 @@ import sys
 import shutil
 
 for dir in sys.path:
-    shutil.copy('./libmahalapy.so', dir)
+    if os.path.isfile(dir+"/libmahalapy.so"):
+        print("-- Up-to-date: "+dir+"/libmahalapy.so")
+    else:
+        shutil.copy('./libmahalapy.so', dir)
+        print("-- Installing: "+dir+"/libmahalapy.so")
